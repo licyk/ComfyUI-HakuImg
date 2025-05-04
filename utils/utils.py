4fc83c16479389df0d98ceff77aa5276550bb726
licyk <licyk233@163.com>
2025-05-04 10:11:58 +0800

    update requirements check

--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,10 @@
-import importlib.metadata
 import os
-import re
-import shutil
 import sys
 import subprocess
 from typing import Optional
 from pathlib import Path
 from .logging import get_logger
+from .check import validate_requirements
 
 
 
@@ -54,161 +52,12 @@ def run_pip(command, desc=None, live=False):
     return run(f'"{sys.executable}" -m pip {command}', desc=f"Installing {desc}", errdesc=f"Couldn't install {desc}", live=live)
 
 
-def get_package_version(pkg_name: str) -> str:
-    try:
-        ver = importlib.metadata.version(pkg_name)
-    except:
-        ver = None
-
-    if ver is None:
-        try:
-            ver = importlib.metadata.version(pkg_name.lower())
-        except:
-            ver = None
-
-    if ver is None:
-        try:
-            ver = importlib.metadata.version(pkg_name.replace("_", "-"))
-        except:
-            ver = None
-
-    return ver
-
-
-def compare_versions(version1: str, version2: str) -> int:
-    try:
-        nums1 = re.sub(r'[a-zA-Z]+', '', version1).replace('-', '.').replace('+', '.').split(".")
-        nums2 = re.sub(r'[a-zA-Z]+', '', version2).replace('-', '.').replace('+', '.').split(".")
-    except:
-        return 0
-
-    for i in range(max(len(nums1), len(nums2))):
-        num1 = int(nums1[i]) if i < len(nums1) else 0
-        num2 = int(nums2[i]) if i < len(nums2) else 0
-
-        if num1 == num2:
-            continue
-        elif num1 > num2:
-            return 1
-        else:
-            return -1
-
-    return 0
-
-
-def is_installed(package, friendly: str = None):
-    #
-    # This function was adapted from code written by vladimandic: https://github.com/vladmandic/automatic/commits/master
-    #
-
-    # Remove brackets and their contents from the line using regular expressions
-    # e.g., diffusers[torch]==0.10.2 becomes diffusers==0.10.2
-    package = re.sub(r'\[.*?\]', '', package)
-
-    try:
-        if friendly:
-            pkgs = friendly.split()
-        else:
-            pkgs = [
-                p
-                for p in package.split()
-                if not p.startswith('-') and not p.startswith('=')
-            ]
-            pkgs = [
-                p.split('/')[-1] for p in pkgs
-            ]   # get only package name if installing from URL
-
-        for pkg in pkgs:
-            pkg = pkg.split(".git")[0] if pkg.endswith(".git") else pkg
-            if '>=' in pkg:
-                pkg_name, pkg_version = [x.strip() for x in pkg.split('>=')]
-            elif '==' in pkg:
-                pkg_name, pkg_version = [x.strip() for x in pkg.split('==')]
-            elif '<=' in pkg:
-                pkg_name, pkg_version = [x.strip() for x in pkg.split('<=')]
-            elif '!=' in pkg:
-                pkg_name, pkg_version = [x.strip() for x in pkg.split('!=')]
-            elif '<' in pkg:
-                pkg_name, pkg_version = [x.strip() for x in pkg.split('<')]
-            elif '>' in pkg:
-                pkg_name, pkg_version = [x.strip() for x in pkg.split('>')]
-            else:
-                pkg_name, pkg_version = pkg.strip(), None
-
-            version = get_package_version(pkg_name)
-
-            if version is not None:
-                if pkg_version is not None:
-                    if '>=' in pkg:
-                        if compare_versions(version, pkg_version) == 1 or compare_versions(version, pkg_version) == 0:
-                            ok = True
-                        else:
-                            ok = False
-                    elif '<=' in pkg:
-                        if compare_versions(version, pkg_version) == -1 or compare_versions(version, pkg_version) == 0:
-                            ok = True
-                        else:
-                            ok = False
-                    elif '!=' in pkg:
-                        if compare_versions(version, pkg_version) != 0:
-                            ok = True
-                        else:
-                            ok = False
-                    elif '>' in pkg:
-                        if compare_versions(version, pkg_version) == 1:
-                            ok = True
-                        else:
-                            ok = False
-                    elif '<' in pkg:
-                        if compare_versions(version, pkg_version) == -1:
-                            ok = True
-                        else:
-                            ok = False
-                    else:
-                        if compare_versions(version, pkg_version) == 0:
-                            ok = True
-                        else:
-                            ok = False
-
-                    if not ok:
-                        return False
-            else:
-                return False
-
-        return True
-    except ModuleNotFoundError:
-        return False
-
-
-def validate_requirements(requirements_file: str):
-    with open(requirements_file, 'r', encoding='utf8') as f:
-        lines = [
-            line.strip()
-            for line in f.readlines()
-            if line.strip() != ''
-            and not line.startswith("#")
-            and not (line.startswith("-") and not line.startswith("--index-url "))
-            and line is not None
-            and "# skip_verify" not in line
-        ]
-
-        for line in lines:
-            if line.startswith("--index-url "):
-                continue
-
-            if not is_installed(line):
-                print(f"{line} not install")
-                return False
-
-        return True
-
-
 def setup_hakuimg():
     logger.info("Check HakuImg requirements")
     requirements_file = os.path.join(Path(__file__).resolve().parent.parent.as_posix(), "requirements.txt")
     if not validate_requirements(requirements_file):
         try:
-            run_pip(f"install -r \"{requirements_file}\"", "Installing HakuImg requirements", live=True)
+            run_pip(f"install -r \"{requirements_file}\"", "HakuImg requirements", live=True)
         except:
             logger.error("Install HakuImg requirements failed, may cause HakuImg not to work")
             return
